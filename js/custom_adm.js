9504284954f661a5707b190ede9d841dda00234c
Luiz Gustavo <luizgustavo55779@gmail.com>
2023-01-09 15:20:55 -0300

    Botão Ações

--- js/custom_adm.js
+++ js/custom_adm.js
@@ -20,3 +20,28 @@ let bars = document.querySelector(".bars");
 bars.addEventListener("click", ()=>{
     sidebar.classList.contains("active") ? sidebar.classList.remove("active") : sidebar.classList.add("active");
 });
+
+// Fim toggle/ bars
+
+function actionDropdown(id){
+    closeDropdownAction();
+    document.getElementById("actionDropdown" + id).classList.toggle("show-dropdown-action");
+}
+
+window.onclick = function(event){
+    if(!event.target.matches(".dropdown-btn-action")){
+        // document.getElementById("actionDropdown").classList.remove("show-dropdown-action");
+        closeDropdownAction();
+    }
+}
+
+function closeDropdownAction(){
+    var dropdown = document.getElementsByClassName("dropdown-action-item");
+    var i;
+    for(i = 0; i< dropdown.length; i++){
+        var openDropdown = dropdown[i];
+        if(openDropdown.classList.contains("show-dropdown-action")){
+            openDropdown.classList.remove("show-dropdown-action");
+        }
+    }
+}
